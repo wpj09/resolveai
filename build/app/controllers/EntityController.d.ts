b9844ea94be1dd43853d82d88b60c1f2b3ce3304
Rodrigo322 <rodrigollrdglucas@gmail.com>
2022-10-11 16:55:09 -0300

    bug resolvido

--- build/app/controllers/EntityController.d.ts
+++
@@ -1,5 +0,0 @@
-import { Request, Response } from "express";
-export declare class EntityController {
-    index(req: Request, res: Response): Promise<Response<any, Record<string, any>>>;
-    store(req: Request, res: Response): Promise<Response<any, Record<string, any>>>;
-}
