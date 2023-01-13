72e7a65e2c76c99aa12fe04c7af4af876eb7fa29
Rodrigo322 <rodrigollrdglucas@gmail.com>
2023-01-13 14:49:09 -0300

    edit prisma

--- src/app/controllers/ProblemController.ts
+++ src/app/controllers/ProblemController.ts
@@ -83,7 +83,7 @@ export class ProblemController {
   async update(req: Request, res: Response) {
     try {
       const { id } = req.params;
-      const { status, solution } = req.body;
+      const { status, solution, entity } = req.body;
 
       const problem = await prisma.problem.findUnique({
         where: { id: Number(id) },
@@ -98,6 +98,7 @@ export class ProblemController {
         data: {
           status,
           solution,
+          entity
         },
       });
       return res.status(200).json(problemUpdated);
